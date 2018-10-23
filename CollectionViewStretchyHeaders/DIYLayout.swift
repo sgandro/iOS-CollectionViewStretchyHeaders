a2e2cfe0bbf76f11d0a5d996c700c161e42f8c7c
Alessandro Perna <almavivadeveloper@MacBook-Pro-di-AlePerna.local>
2018-10-23 16:58:25 +0200

    first commit

---
+++ CollectionViewStretchyHeaders/DIYLayout.swift
@@ -0,0 +1,9 @@
+//
+//  DYILayout.swift
+//  CollectionViewStretchyHeaders
+//
+//  Created by Alessandro Perna on 06/06/18.
+//  Copyright © 2018 VJTechnology Srl. All rights reserved.
+//
+
+import Foundation
